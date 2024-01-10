c2e86b3c8763ff87023fefcb8ef4d8e0cbfd3e87
Michael Primavera <pastasauce59@gmail.com>
2024-01-10 00:06:35 -0500

    Created check_flights method in FlightSearch class.

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,6 @@
 import requests
 from keys import *
+from flight_data import FlightData
 
 TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com"
 
@@ -18,3 +19,17 @@ class FlightSearch:
         code = response.json()["locations"][0]["code"]
         return code
         # print(code)
+    
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"apikey": TEQUILA_API_KEY}
+        tequila_config = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time,
+            "date_to": to_time,
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "one_for_city": 1,
+            "curr": "USD",
+            "max_stopovers": 0
+        }
